37e789baeef203150a8ff2c196e4a4b9248d8c25
kalia <kaliarovich93@gmail.com>
2023-01-25 01:28:31 +0200

    added posts from textarea

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,23 @@
 import classes from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
-
+import React from "react";
 
 
 const Dialogs = (props) => {
 
-
     let dialogsElements = props.state.dialogs.map(elem => {
         return <DialogItem name={elem.name} id={elem.id}/>
     })
-
     let messagesElements = props.state.messages.map(elem => {
         return <Message message={elem.message} id={elem.id}/>
     })
+    let newMessageElement = React.createRef();
+    let showMessage = () => {
+        let message = newMessageElement.current.value;
+        alert(message);
+    }
+
 
     return (
         <div className={classes.dialogs}>
@@ -22,7 +26,16 @@ const Dialogs = (props) => {
             </div>
             <div className={classes.messages}>
                 {messagesElements}
+                <div>
+                    <div>
+                        <div><textarea ref={newMessageElement} cols="50" rows="5"></textarea></div>
+                        <div>
+                            <button onClick={showMessage}>ADD</button>
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </div>
 
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,21 +1,25 @@
 import classes from './MyPosts.module.css'
 import Post from "./Post/Post";
-
+import React from "react";
 
 const MyPosts = (props) => {
     let postsElements = props.posts.map(elem => {
         return <Post message={elem.message} id={elem.id} likeCounts={elem.likesCount}/>
     })
-
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        props.addPost(text);
+    }
+    let newPostElement = React.createRef()
 
     return (
         <div className={classes.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <div><textarea name="" id="" cols="50" rows="5"></textarea></div>
+                    <div><textarea ref={newPostElement} cols="50" rows="5"></textarea></div>
                     <div>
-                        <button>ADD Post</button>
+                        <button onClick={addPost}>ADD Post</button>
                     </div>
                 </div>
                 Newpost
